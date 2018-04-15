9471d67b0112e0d77a1bc8887eacddbdf2f55329
Raurur00 <javierzavalatorres@gmail.com>
2018-04-15 19:41:27 -0300

    Graphic Interface

---
+++ interface.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from main import *
+
+def imprimir():
+	print(load_yahoo_quote(empresa.get(),fechaInicio.get(),fechaTermino.get()))
+
+ventana = Tk()
+ventana.title("Cateando")
+ventana.geometry("400x400")
+
+empresa = StringVar()
+fechaInicio = StringVar()
+fechaTermino = StringVar() 
+
+empresaLabel= Label(ventana,text="Empresa").place(x=10,y=10)
+empresaEntry = Entry(ventana,textvariable=empresa).place(x=120,y=10)
+
+fechaInicioLabel= Label(ventana,text="Fecha Inicio").place(x=10,y=40)
+fechaInicioEntry = Entry(ventana,textvariable=fechaInicio).place(x=120,y=40)
+
+fechaTerminoLabel = Label(ventana,text="Fecha Termino").place(x=10,y=70)
+fechaTerminoEntry = Entry(ventana,textvariable=fechaTermino).place(x=120,y=70)
+
+boton = Button(ventana,text="Enviar",command=imprimir).place(x=10,y=100)
+
+ventana.mainloop()
