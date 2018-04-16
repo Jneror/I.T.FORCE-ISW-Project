2db5047b16b9f14d309102e60936884eb1350fd1
Raurur00 <javierzavalatorres@gmail.com>
2018-04-16 00:16:15 -0300

    Otros inputs agregados

--- interface.py
+++ interface.py
@@ -15,7 +15,11 @@ ventana.geometry("400x400")
 
 empresa = StringVar()
 fechaInicio = StringVar()
-fechaTermino = StringVar() 
+fechaTermino = StringVar()
+T = StringVar()
+n = StringVar()
+r = StringVar()
+strike = StringVar()
 
 empresaLabel= Label(ventana,text="Empresa").place(x=10,y=10)
 empresaEntry = Entry(ventana,textvariable=empresa).place(x=120,y=10)
@@ -26,6 +30,18 @@ fechaInicioEntry = Entry(ventana,textvariable=fechaInicio).place(x=120,y=40)
 fechaTerminoLabel = Label(ventana,text="Fecha Termino").place(x=10,y=70)
 fechaTerminoEntry = Entry(ventana,textvariable=fechaTermino).place(x=120,y=70)
 
-boton = Button(ventana,text="Enviar",command=imprimir).place(x=10,y=100)
+strikeLabel = Label(ventana, text="Strike").place(x=10,y=100)
+strikeEntry = Entry(ventana, textvariable=strike).place(x=120,y=100)
+
+TLabel = Label(ventana, text="Tiempo de Maduracion").place(x=10,y=130)
+TEntry = Entry(ventana, textvariable=T).place(x=150,y=130)
+
+nLabel = Label(ventana, text="Numero de simulaciones").place(x=10,y=160)
+nEntry = Entry(ventana, textvariable=n).place(x=150,y=160)
+
+rLabel = Label(ventana, text="r").place(x=10,y=190)
+rEntry = Entry(ventana, textvariable=r).place(x=120,y=190)
+
+boton = Button(ventana,text="Enviar",command=imprimir).place(x=10,y=220)
 
 ventana.mainloop()
