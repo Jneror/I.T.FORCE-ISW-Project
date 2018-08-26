5eb62ffc8d65d0f7eb103c7012ca7421c45bf1ad
Raurur00 <javierzavalatorres@gmail.com>
2018-08-25 22:24:40 -0300

    GUI 2.0

--- ISW/GUI.py
+++ ISW/GUI.py
@@ -1,11 +1,11 @@
-import kivy
-kivy.require('1.9.0')
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager,Screen
 from Option import Option
 import matplotlib.pyplot as plt
 
-class Form(FloatLayout):
+import os
+
+class FormScreen(Screen):
     
     def sim(self,strike,timeMadurity,numSim,riskFreeRate,company,source):
         opt = Option(float(strike),int(timeMadurity),int(numSim),float(riskFreeRate),company,source)
@@ -17,11 +17,13 @@ class Form(FloatLayout):
         plt.hist(endValues, 40)
         plt.show()
 
+class Manager(ScreenManager):
+    form_screen = ObjectProperty(None)
+    graphic_screen = ObjectProperty(None)
 
 class GUIApp(App):
-
     def build(self):
-        return Form()
+        return Manager()
 
 GUI = GUIApp()
 
